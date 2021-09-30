1a88af1167c060227e5376cf5478df6588675019
tarunoptiproerp <tsarsode@optiproerp.com>
2021-09-30 12:38:48 +0530

    My_dotnet_demo_project

---
+++ Frist_Test/Program.cs
@@ -0,0 +1,34 @@
+﻿// using System;
+
+// namespace Frist_Test
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             Console.WriteLine("Hello World!");
+//         }
+//     }
+// }
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Threading;
+namespace selenium
+{
+class Program
+{
+static void Main(string[] args)
+{
+FirefoxBinary binary = new FirefoxBinary();
+FirefoxOptions options = new FirefoxOptions();//optional
+options.BrowserExecutableLocation = @"C:\Program Files\Mozilla Firefox\firefox.exe";
+IWebDriver driver = new FirefoxDriver(options);
+
+driver.Navigate().GoToUrl("https://www.google.com");
+driver.FindElement(By.Name("q")).SendKeys("Fleek It solutions"+ Keys.Return);
+Thread.Sleep(2000);
+driver.Quit();
+}
+}
+}
