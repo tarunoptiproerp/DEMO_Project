8b226f000c77c17014614085b72a2ff6c3cdc734
tarunoptiproerp <tsarsode@optiproerp.com>
2021-10-01 19:05:25 +0530

    abc test

--- Frist_Test/Program.cs
+++ Frist_Test/Program.cs
@@ -15,23 +15,30 @@ using OpenQA.Selenium.Firefox;
 using System;
 using System.Threading;
 
+namespace Frist_Test
+{
+
 public class Program
 {
 static void Main(string[] args)
-
 {
+    abc();
+}
 
-FirefoxBinary binary = new FirefoxBinary();
-FirefoxOptions options = new FirefoxOptions();//optional
-options.BrowserExecutableLocation = @"C:\Program Files\Mozilla Firefox\firefox.exe";
-IWebDriver driver = new FirefoxDriver(options);
+private static void abc()
+{    
+    FirefoxBinary binary = new FirefoxBinary();
+    FirefoxOptions options = new FirefoxOptions();//optional
+    options.BrowserExecutableLocation = @"C:\Program Files\Mozilla Firefox\firefox.exe";
+    IWebDriver driver = new FirefoxDriver(options);
 
-driver.Navigate().GoToUrl("https://opensource-demo.orangehrmlive.com/");
-driver.FindElement(By.Name("txtUsername")).SendKeys("Admin");
-driver.FindElement(By.Name("txtPassword")).SendKeys("admin123");
-driver.FindElement(By.Name("Submit")).Click();
-Thread.Sleep(2000);
-driver.Quit();
-    
+    driver.Navigate().GoToUrl("https://opensource-demo.orangehrmlive.com/");
+    driver.FindElement(By.Name("txtUsername")).SendKeys("Admin");
+    driver.FindElement(By.Name("txtPassword")).SendKeys("admin123");
+    driver.FindElement(By.Name("Submit")).Click();
+    Thread.Sleep(2000);
+    driver.Quit();
+}
+
 }
 }
