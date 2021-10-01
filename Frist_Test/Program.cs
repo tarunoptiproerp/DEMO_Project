76a921d06261c70df9831f4bb2800bcba3472639
tarunoptiproerp <tsarsode@optiproerp.com>
2021-10-01 13:56:23 +0530

    my latest 10/01/2021  1:56PM

--- Frist_Test/Program.cs
+++ Frist_Test/Program.cs
@@ -14,12 +14,13 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
 using System;
 using System.Threading;
-namespace selenium
-{
-class Program
+
+public class Program
 {
 static void Main(string[] args)
 {
+    
+
 FirefoxBinary binary = new FirefoxBinary();
 FirefoxOptions options = new FirefoxOptions();//optional
 options.BrowserExecutableLocation = @"C:\Program Files\Mozilla Firefox\firefox.exe";
@@ -29,6 +30,6 @@ driver.Navigate().GoToUrl("https://www.google.com");
 driver.FindElement(By.Name("q")).SendKeys("Fleek It solutions"+ Keys.Return);
 Thread.Sleep(2000);
 driver.Quit();
-}
+    
 }
 }
